fd9b4037f93987fb09845155e6bf2937b9f395dc
Koohyewon <khw6746@naver.com>
2024-06-01 02:32:37 +0900

    feat : firstSection nav

--- js/firstSection.js
+++ js/firstSection.js
@@ -1,9 +1,22 @@
 //top 우측 상단의 KOR 버튼을 클릭하면 language_list가 보이게 설정
-document.getElementById('languageBtn').addEventListener('click', function() {
+/*document.getElementById('languageBtn').addEventListener('click', function() {
     var languageList = document.querySelector('.language_list');
     if (languageList.classList.contains('show')) {
         languageList.classList.remove('show');
     } else {
         languageList.classList.add('show');
     }
+});*/
+
+//top 우측 상단의 KOR 버튼을 클릭하면 language_list가 보이게 설정
+document.getElementById('languageBtn').addEventListener('click', function() {
+    var languageList = document.querySelector('.language_list');
+    if (languageList.classList.contains('hidden')) {
+        languageList.classList.remove('hidden');
+        languageList.style.maxHeight = languageList.scrollHeight + 'px';
+    } else {
+        languageList.style.maxHeight = '0';
+        languageList.classList.add('hidden');
+    }
 });
+

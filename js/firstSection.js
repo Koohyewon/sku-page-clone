feb0797e7018ff915af7aa417a8442680c3afc00
Koohyewon <khw6746@naver.com>
2024-06-01 23:02:04 +0900

    style: add prettier

--- js/firstSection.js
+++ js/firstSection.js
@@ -9,14 +9,13 @@
 });*/
 
 //top 우측 상단의 KOR 버튼을 클릭하면 language_list가 보이게 설정
-document.getElementById('languageBtn').addEventListener('click', function() {
-    var languageList = document.querySelector('.language_list');
-    if (languageList.classList.contains('hidden')) {
-        languageList.classList.remove('hidden');
-        languageList.style.maxHeight = languageList.scrollHeight + 'px';
-    } else {
-        languageList.style.maxHeight = '0';
-        languageList.classList.add('hidden');
-    }
+document.getElementById("languageBtn").addEventListener("click", function () {
+  var languageList = document.querySelector(".language_list");
+  if (languageList.classList.contains("hidden")) {
+    languageList.classList.remove("hidden");
+    languageList.style.maxHeight = languageList.scrollHeight + "px";
+  } else {
+    languageList.style.maxHeight = "0";
+    languageList.classList.add("hidden");
+  }
 });
-

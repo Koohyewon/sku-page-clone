af70ff763f466fec5913273aee1189265bb84677
Koohyewon <khw6746@naver.com>
2024-06-21 13:06:25 +0900

    nav list 추가

--- js/firstSection.js
+++ js/firstSection.js
@@ -1,14 +1,4 @@
 //top 우측 상단의 KOR 버튼을 클릭하면 language_list가 보이게 설정
-/*document.getElementById('languageBtn').addEventListener('click', function() {
-    var languageList = document.querySelector('.language_list');
-    if (languageList.classList.contains('show')) {
-        languageList.classList.remove('show');
-    } else {
-        languageList.classList.add('show');
-    }
-});*/
-
-//top 우측 상단의 KOR 버튼을 클릭하면 language_list가 보이게 설정
 document.getElementById("languageBtn").addEventListener("click", function () {
   var languageList = document.querySelector(".language_list");
   if (languageList.classList.contains("hidden")) {
@@ -18,4 +8,16 @@ document.getElementById("languageBtn").addEventListener("click", function () {
     languageList.style.maxHeight = "0";
     languageList.classList.add("hidden");
   }
+});
+
+//nav에 마우스 호버 시 로고 이미지를 변경
+const nav = document.querySelector(".nav");
+const logoImg = document.querySelector(".logo-img");
+
+nav.addEventListener("mouseover", function () {
+  logoImg.src = "img/logo_blue.png";
+});
+
+nav.addEventListener("mouseout", function () {
+  logoImg.src = "img/logo_white.png";
 });

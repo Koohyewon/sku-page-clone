add1920a427dc4e1e0d08ab38203d5b69806c2e9
eunah112 <shdmsdk2012@gmail.com>
2024-06-22 12:38:19 +0900

    test.html 삭제

--- js/AboutSKU.js
+++ js/AboutSKU.js
@@ -1,26 +1,20 @@
 const AboutSKUIMG = [
     {
-        "index": 1,
         "img": d,
     },
     {
-        "index": 2,
         "img": d,
     },
     {
-        "index": 3,
         "img": d,
     },
     {
-        "index": 4,
         "img": d,
     },
     {
-        "index": 5,
         "img": d,
     },
     {
-        "index": 6,
         "img": d,
     },
 ];
